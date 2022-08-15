aac12ac0e59d5e71077ebdd86302f999e71d1fd6
AquaApple <yassershehab9@gmail.com>
2022-08-15 22:00:19 +0200

    TypeAnnotationsWithArrays

---
+++ TypeAnnotationsWithArrays.ts
@@ -0,0 +1,15 @@
+/*
+Type Annotations with Arrays  
+*/
+
+let all: string | number | boolean;
+
+all = "A";
+all = 100;
+all = true;
+
+// let myFriends: string[] = ["Friend1", "Friend2", "Friend3", 1];
+
+for (let i = 0; i < myFriends.length; i++) {
+  console.log(myFriends[i].repeat(3));
+}
